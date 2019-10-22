2b8449d1f9c7f7262b187091bdeddf15fdaaee1c
JamesDream <1741704351@qq.com>
2019-10-22 20:24:08 +0800

    update

--- price/getPrice.py
+++ price/getPrice.py
@@ -14,29 +14,40 @@ def loop(List):
   year = datetime.datetime.now().year
   month = datetime.datetime.now().month
   day = datetime.datetime.now().day
-
+  MarketNull = []
   for each in List:
     url = init(each, year, month, day)
     # 爬取网页信息返回未清理的参数
-    Vdata,Vmarket,Vname,Vlow,Vhigh = getHtml(url)
-    # 返回清理的参数
-    date,mark,name,low,high = ClearData(Vdata,Vmarket,Vname,Vlow,Vhigh)
-    # 转为csv
-    df = {
-      "日期": date,
-      "市场名称": mark,
-      "蔬菜名": name,
-      "最低价": low,
-      "最高价": high
-    }
-    df = pd.DataFrame(df)
-    df.to_csv(f'./dataset/price-{each}-{year}{month}{day}.csv',index=False)
+    Vdate,Vmarket,Vname,Vlow,Vhigh = getHtml(url)
+    # 判断是否为空数据，空数据就直接跳过本次循环
+    if(len(Vname)<=0):
+      print('本市场ID没有数据')
+      MarketNull.append(each)
+      df = {
+        "MarketId": MarketNull
+      }
+      df = pd.DataFrame(df)
+      df.to_csv(f'./dataset/MarketNULL.csv',index=False)
+
+    else:
+      # 返回清理的参数
+      date,mark,name,low,high = ClearData(Vdate,Vmarket,Vname,Vlow,Vhigh)
+      # 转为csv
+      df = {
+        "日期": date,
+        "市场名称": mark,
+        "蔬菜名": name,
+        "最低价": low,
+        "最高价": high
+      }
+      df = pd.DataFrame(df)
+      df.to_csv(f'./dataset/price-{each}-{year}{month}{day}.csv',index=False)
 
 #获取是否含有多页数据
 def getHtml(url):
   page=requests.Session().get(url) 
   soup=html.fromstring(page.text)
-  Vdata,Vmarket,Vname,Vlow,Vhigh = setData(soup)
+  Vdate,Vmarket,Vname,Vlow,Vhigh = setData(soup)
   # 获取是否有下一页信息
   next = is_next(soup)
   # 如果存在下一页信息
@@ -46,33 +57,33 @@ def getHtml(url):
       page=requests.Session().get(f'http://www.vegnet.com.cn/{each}') 
       soup=html.fromstring(page.text)
       Ddata,Dmarket,Dname,Dlow,Dhigh = setData(soup)
-      Vdata.extend(Ddata)
+      Vdate.extend(Ddata)
       Vmarket.extend(Dmarket)
       Vname.extend(Dname)
       Vlow.extend(Dlow)
       Vhigh.extend(Dhigh)
   
   #将数据整理为一定的格式
-  return Vdata,Vmarket,Vname,Vlow,Vhigh
+  return Vdate,Vmarket,Vname,Vlow,Vhigh
 
 # 筛选出数据
 def setData(soup):
-  Vdata = soup.xpath('//div[@class="pri_k"]//p//span[@class="k_1"]/text()')
+  Vdate = soup.xpath('//div[@class="pri_k"]//p//span[@class="k_1"]/text()')
   Vmarket = soup.xpath('//div[@class="pri_k"]//p//span[@class="k_3"]//a/text()')
   Vname = soup.xpath('//div[@class="pri_k"]//p//span[@class="k_2"][1]/text()')
   Vlow = soup.xpath('//div[@class="pri_k"]//p//span[@class="k_2"][2]/text()')
   Vhigh = soup.xpath('//div[@class="pri_k"]//p//span[@class="k_2"][3]/text()')
 
-  return Vdata,Vmarket,Vname,Vlow,Vhigh
+  return Vdate,Vmarket,Vname,Vlow,Vhigh
 
-def ClearData(Vdata,Vmarket,Vname,Vlow,Vhigh):
+def ClearData(Vdate,Vmarket,Vname,Vlow,Vhigh):
   date = []
   high = []
   low = []
   mark = []
   name = []
 
-  for each in Vdata:
+  for each in Vdate:
     each = each.__str__()
     each = each.strip('[')
     each = each.strip(']')
